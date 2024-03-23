89707ab4769850acab1de30aeb276dfa31ffd363
petervidaHU <peter_vida1@epam.com>
2024-03-24 00:00:49 +0100

    test coverage

--- 5-testing/jest.config.js
+++ 5-testing/jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
     testEnvironment: 'node',
     roots: ['./src'],
     preset: 'ts-jest',
+    collectCoverageFrom: ['src/**'],
+    coverageReporters: ['html'],
+    verbose: true,
+    coverageThreshold: {
+        global: {
+          lines: 85
+        }
+    }
 };
